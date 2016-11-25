a55b5f3c40c4334a7479d6ec24c481aa30a2b6f1
Paula Loeblein <paulaloebleinh2o@gmail.com>
2016-11-25 11:39:03 -0300

    Add files via upload

---
+++ src/ifroteca/SingletonConexao.java
@@ -0,0 +1,40 @@
+package ifroteca;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+	public class SingletonConexao {
+	private static SingletonConexao instance;
+	private Connection conexao;
+	private Statement stm;
+	private String url = "jdbc:mysql://localhost/ifroteca",
+				   usuario = "root",
+				   senha = "root";
+	
+	private SingletonConexao(){
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+    		this.conexao = DriverManager.getConnection(url, usuario, senha);
+    		stm = conexao.createStatement();
+		} catch (SQLException sqle) {
+	         sqle.printStackTrace();
+	      } catch (ClassNotFoundException cnfe) {
+	         cnfe.printStackTrace();
+	      } 
+	   }
+	
+	private static SingletonConexao getSingleton(){
+		if (instance == null){
+			instance = new SingletonConexao();
+		}
+		return instance;
+	}
+	
+	public Connection getConexao(){
+		return this.conexao;
+	}
+
+}
