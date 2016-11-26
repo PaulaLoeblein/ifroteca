252731a3af1fb9c8c14533fff6fa0c48e858dc64
Paula Loeblein <paulaloebleinh2o@gmail.com>
2016-11-26 19:27:32 -0400

    Add files via upload

---
+++ src/ifroteca/Pessoa.java
@@ -0,0 +1,88 @@
+package ifroteca;
+
+public class Pessoa {
+	    private String nome;
+	    private String telefone; 
+	    private String dataDeNascimento;
+	    private String endereco;
+	    private String cidade;
+	    private String cpf;
+	    private String rg;
+	    private String email;
+	   
+	    public String getNome() {
+	        return nome;
+	    }
+
+	   
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+
+	    public String getTelefone() {
+	        return telefone;
+	    }
+
+	   
+	    public void setTelefone(String telefone) {
+	        this.telefone = telefone;
+	    }
+	    
+	    
+	    public String getDataDeNascimento() {
+	        return dataDeNascimento;
+	    }
+
+	    public void setDataDeNascimento(String dataDeNascimento) {
+	        this.dataDeNascimento = dataDeNascimento;
+	    }
+	    
+	 
+	    public String getEndereco() {
+	        return endereco;
+	    }
+
+	    public void setEndereco(String endereco) {
+	        this.endereco = endereco;
+	    }
+	    
+	 
+	    public String getCidade() {
+	        return cidade;
+	    }
+
+	    public void setCidade(String cidade) {
+	        this.cidade = cidade;
+	    }
+
+	    
+	    public String getCpf() {
+	        return cpf;
+	    }
+
+	  
+	    public void setCpf(String cpf) {
+	        this.cpf = cpf;
+	    }
+
+	  
+	    public String getRg() {
+	        return rg;
+	    }
+
+	  
+	    public void setRg(String rg) {
+	        this.rg = rg;
+	    }
+	    
+	   
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	 
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+	}
