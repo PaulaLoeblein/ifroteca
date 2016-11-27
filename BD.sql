5dae730894fc0d7ed29c18d5ed812dfd66b517cd
Paula Loeblein <paulaloebleinh2o@gmail.com>
2016-11-27 12:41:27 -0300

    Update BD.sql

--- BD.sql
+++ BD.sql
@@ -30,10 +30,11 @@ primary key (cod_bib)
 );
 create table livro(
 cod_liv int auto_increment not null,
-titulo_liv varchar (40),
+titulo_liv varchar (100),
 autor_liv varchar (40),
 editora_liv varchar (40),
 quantidade_liv int,
+anoPubli_liv varchar (10),
 dataCadastro_liv date,
 idioma_liv varchar (20),
 codigoLivro_liv int,
@@ -41,63 +42,25 @@ genero_liv varchar (30),
 areaEnsino_liv varchar (100),
 primary key (cod_liv)
 );
-create table reserva(
-cod_res int auto_increment not null,
-dataReserva date not null,
-dataPrevistaEmp date not null,
-cod_alu int not null,
-cod_bib int not null,
-foreign key (cod_alu) references aluno (cod_alu),
-foreign key (cod_bib) references bibliotecario (cod_bib),
-primary key (cod_res)
-);
-create table livro_reserva(
-cod_lr int auto_increment not null,
-quant_lr int not null,
-cod_liv int not null,
-cod_res int not null,
-foreign key (cod_liv) references livro (cod_liv),
-foreign key (cod_res) references reserva (cod_res),
-primary key (cod_lr)
-);
+
 create table emprestimo(
 cod_emp int auto_increment not null,
 dataEmprestimo_emp date not null,
 dataPrevistaDev_emp date not null,
-cod_res int not null,
 cod_bib int not null,
 cod_alu int not null,
-foreign key (cod_res) references reserva (cod_res),
+cod_liv int,
 foreign key (cod_bib) references bibliotecario (cod_bib),
 foreign key (cod_alu) references aluno (cod_alu),
+foreign key (cod_liv) references livro (cod_liv),
 primary key (cod_emp)
 );
-create table livros_emprestimo(
-cod_livemp int auto_increment not null,
-quant_livemp int not null,
-cod_liv int not null,
-cod_emp int not null,
-foreign key (cod_liv) references livro (cod_liv),
-foreign key (cod_emp) references emprestimo (cod_emp),
-primary key (cod_livemp)
-);
+
 create table devolucao(
 cod_dev int auto_increment not null,
 dataEmprestimo_dev date not null,
 dataDevolucao_dev date not null,
-valorMulta_dev float,
 cod_emp int not null,
-cod_alu int not null,
 foreign key (cod_emp) references emprestimo (cod_emp),
-foreign key (cod_alu) references aluno (cod_alu),
 primary key (cod_dev)
-);
-create table livros_devolucao(
-cod_livdev int auto_increment not null,
-quant_livdev int not null,
-cod_liv int not null,
-cod_dev int not null,
-foreign key (cod_liv) references livro (cod_liv),
-foreign key (cod_dev) references devolucao (cod_dev),
-primary key (cod_livdev)
 );
